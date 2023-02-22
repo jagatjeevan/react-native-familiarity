c74feae1fb99d53e9f2b3edc5c5619009e4658cc
Jagat Jeevan Sahoo <jagatsah.TW@apollo.io>
2023-02-22 13:20:16 +0530

    - Create application with expo - Create src folder and add entry point in app.json - Add prettier files - Export registeredComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default registerRootComponent(App);
